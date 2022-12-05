fa5aa24f07d60366e83683b9250bce3a838dd667
cballing <cballingucsd@gmail.com>
2022-12-05 15:15:35 -0800

    baseline class and tests JS from class

---
+++ index.js
@@ -0,0 +1,8 @@
+//bring in Inquirer
+
+//employee = [{Manager}, {Engineer}, {Intern}]
+
+//create a fxn that runs Inquirer prompt questions .then(() => inquirer.prompt()then)
+//chain promises - run another Inquirer inside the .then
+
+//end point = loop employee array = convert into html
